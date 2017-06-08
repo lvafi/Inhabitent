cb1c8a60f70377b4700bf51bff53f147a62d9dd5
vf <l_vafi@yahoo.com>
2017-06-08 15:12:19 -0700

    edited the css for the front page

--- plugins/inhabitent-functionality/lib/functions/post-types.php
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -61,4 +61,61 @@ function register_products_post_type() {
 add_action( 'init', 'register_products_post_type', 0 );
 
 
+// Register Custom Post Type
+function register_adventures_post_type() {
+
+	$labels = array(
+		'name'                  => 'Adventures',
+		'singular_name'         => 'Adventure',
+		'menu_name'             => 'Adventures',
+		'name_admin_bar'        => 'Adventure',
+		'archives'              => 'Adventure Archives',
+		'attributes'            => 'Adventure Attributes',
+		'parent_item_colon'     => 'Adventures Parent Item:',
+		'all_items'             => 'All Adventures',
+		'add_new_item'          => 'Add New Adventures',
+		'add_new'               => 'Add New Adventure',
+		'new_item'              => 'New Adventures',
+		'edit_item'             => 'Edit Adventure',
+		'update_item'           => 'Update Adventure',
+		'view_item'             => 'View Adventure',
+		'view_items'            => 'View Adventures',
+		'search_items'          => 'Search Adventures',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Adventure',
+		'uploaded_to_this_item' => 'Uploaded to this Adventure',
+		'items_list'            => 'Adventures list',
+		'items_list_navigation' => 'Adventures list navigation',
+		'filter_items_list'     => 'Filter Adventures list',
+	);
+	$args = array(
+		'label'                 => 'Adventures',
+		'description'           => 'Adventures are adventures.',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 20,
+		'menu_icon'             => 'dashicons-carrot',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'adventures', $args );
+
+}
+add_action( 'init', 'register_adventures_post_type', 0 );
+
+
 
